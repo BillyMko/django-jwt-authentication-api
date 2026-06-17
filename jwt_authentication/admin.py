4da445f0b147c5f25ad4d3d35a826974511164df
BelieveMko <mukobelieve@gmail.com>
2026-06-17 22:23:19 +0200

    Configure CustomUserAdmin with list_display and fieldsets

--- jwt_authentication/admin.py
+++ jwt_authentication/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import EmailVerificationToken
+from django.contrib.auth.admin import UserAdmin
+from .models import EmailVerificationToken, User
 
 @admin.register(EmailVerificationToken)
 class EmailVerificationTokenAdmin(admin.ModelAdmin):
     list_display = ["user", "token", "created_at"]
     readonly_fields = ["token", "created_at"]
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "username", "role", "is_verified", "is_staff",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Fields",
+         {
+             "fields":(
+                 "role",
+                 "is_verified",
+                 )
+                 },
+                 ),
+)
