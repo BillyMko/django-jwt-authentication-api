1165fa7f0d2dbff43f2211ca960a8aa30c9926a1
BelieveMko <mukobelieve@gmail.com>
2026-06-17 22:16:51 +0200

    Add custom user role-based permissions

---
+++ jwt_authentication/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return (request.user and
+                request.user.is_authenticated and
+                request.user.role == "admin"
+                )
+    
+class IsPremium(BasePermission):
+    def has_permission(self, request, view):
+        return (request.user and
+                request.user.is_authenticated and
+                getattr(request.user, "role", None) in ("admin", "premium")
+                )
+    
+class IsOwnerOrAdmin(BasePermission):
+   def has_object_permission(self, request, view, obj):
+        if request.user.role == "admin":
+            return True
+        
+        return obj.user == request.user 
