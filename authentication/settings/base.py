45d0b490099a44b9a9ae7d58a035cce9d2dc6191
BelieveMko <mukobelieve@gmail.com>
2026-06-19 23:49:49 +0200

    Configure token life times and rotation settings

--- authentication/settings/base.py
+++ authentication/settings/base.py
@@ -142,9 +142,15 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": True,
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+PASSWORD_RESET_EXPIRY_HOURS = 2
